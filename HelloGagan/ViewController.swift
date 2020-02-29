83db1775f4ac9c519eeab49311ca922305e070b0
gagandeepkaur <sherry2312.gk@gmail.com>
2020-02-28 21:20:38 -0500

    initial commit

--- HelloGagan/ViewController.swift
+++ HelloGagan/ViewController.swift
@@ -8,13 +8,36 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class FirstViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    @IBOutlet weak var WELCOMESHERRY: UILabel!
+     override func viewDidAppear(_ animated: Bool) {
+            print("View DidAppear")
+        }
+        
+        override func viewWillAppear(_ animated: Bool) {
+            print("view WillAppear")
+        }
+        
+        override func viewDidDisappear(_ animated: Bool) {
+            print("view Did Disappear")
+        }
+        
+        override func viewWillDisappear(_ animated: Bool) {
+            print("View WillDisappear")
+        }
+        override func viewDidLoad() {
+            super.viewDidLoad()
+           WELCOMESHERRY.text = " Welcome sherry  to IOS Programming"
+            
+            print(WELCOMESHERRY.text!)
+            
+        }
+        
+        @IBAction func btnClickMeTap(_ sender: UIButton)
+        {
+          WELCOMESHERRY.text = "ohh Dont click me"
+        }
+        
     }
-
-
-}
-
+   
